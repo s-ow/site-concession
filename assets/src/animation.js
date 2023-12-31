5ae9d65b91e2b41b663ce9a3be4764fbf26c1432
s-ow <spgc.paypal@gmail.com>
2023-12-31 18:41:49 +0100

    fix anim

--- assets/src/animation.js
+++ assets/src/animation.js
@@ -8,6 +8,7 @@ function handleScrollAnimation() {
 }
 
 window.addEventListener('scroll', handleScrollAnimation);
+window.addEventListener('touchmove', handleScrollAnimation);
 
 document.addEventListener('DOMContentLoaded', handleScrollAnimation);
 
@@ -16,7 +17,7 @@ function isElementInViewport(el) {
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 100 &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
